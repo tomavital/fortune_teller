8eaa38807c8b0182e077a8b9b9e83178b39d65eb
rails -v <c:sites>
2017-04-21 13:02:12 -0500

    cncer

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
 
   get("/zodiacs/aries",       { :controller =>  "zodiacs", :action =>  "ram" })
   get("/zodiacs/taurus",      { :controller =>  "zodiacs", :action =>  "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
+  get("/zodiacs/gemini",        { :controller => "zodiacs", :action => "twins" })
+  get("/zodiacs/cancer",       { :controller => "zodiacs",  :action => "crab" })
   # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
   # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
   # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
